7bb292d435cceb78e22a09245d21ec16cbabd152
Jakub Choroszucha <jakub.rrk@outlook.com>
2022-09-21 14:47:39 +0200

    change functions

--- script.js
+++ script.js
@@ -10,47 +10,60 @@ function getPlayerChoice() {
 }
 
 function playRound(playerChoice, computerChoice) {
-  let outcome = result();
-  if (outcome == "win") return `You win!\n${playerChoice.charAt(0).toUpperCase()+playerChoice.slice(1)} beats ${computerChoice}!`;
-  if (outcome == "lose") return `You lose!\n${computerChoice.charAt(0).toUpperCase()+computerChoice.slice(1)} beats ${playerChoice}!`;
-  if (outcome == "draw") return "It's a draw!";
-
-
-  function result() {
-    switch (playerChoice) {
-      case "rock":
-        switch (computerChoice) {
-          case "rock":
-            return "draw";
-          case "paper":
-            return "lose";
-          case "scissors":
-            return "win";
-        }
-      case "paper":
-        switch (computerChoice) {
-          case "rock":
-            return "win";
-          case "paper":
-            return "draw";
-          case "scissors":
-            return "lose";
-        }
-      case "scissors":
-        switch (computerChoice) {
-          case "rock":
-            return "lose";
-          case "paper":
-            return "win";
-          case "scissors":
-            return "draw";
-        }
-    }
+  switch (playerChoice) {
+    case "rock":
+      switch (computerChoice) {
+        case "rock":
+          return "draw";
+        case "paper":
+          return "lose";
+        case "scissors":
+          return "win";
+      }
+    case "paper":
+      switch (computerChoice) {
+        case "rock":
+          return "win";
+        case "paper":
+          return "draw";
+        case "scissors":
+          return "lose";
+      }
+    case "scissors":
+      switch (computerChoice) {
+        case "rock":
+          return "lose";
+        case "paper":
+          return "win";
+        case "scissors":
+          return "draw";
+      }
   }
 }
 
 function game() {
+  let score0 = 0;
+  let score1 = 0;
+
   for (let i = 0; i<5; i++) {
-    
+    let outcome = playRound(getPlayerChoice(), getComputerChoice());
+    console.log(outcome)
+    if (outcome == "win") {
+      console.log(`You win!\n${playerChoice.charAt(0).toUpperCase()+playerChoice.slice(1)} beats ${computerChoice}!`);
+      score0 += 1;
+    }
+    else if (outcome == "lose") {
+      console.log(`You lose!\n${computerChoice.charAt(0).toUpperCase()+computerChoice.slice(1)} beats ${playerChoice}!`);
+      score1 += 1;
+    }
+    else if (outcome == "draw") {
+      console.log("It's a draw!");
+      score0 += 0.5;
+      score1 += 0.5;
+    }
+    else console.log("no outcome???")
   }
+  if (score0 > score1) alert("You win!!!");
+  else if (score0 < score1) alert("You lose!!!");
+  else if (score0 == score1) alert("It's a draw. Nobody wins, everyone loses.");
 }
